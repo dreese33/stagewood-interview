2894a408d2fdecc76fcc1346183d808d5c14ab15
dreese33 <dylanjacobreese@gatech.edu>
2020-12-26 21:21:47 -0500

    Attempted to add authentication in the database for more security, but will use JWT instead on next commit for now since this is not working

--- src/database/queries.js
+++ src/database/queries.js
@@ -43,3 +43,70 @@ const pgcryptoExtension = `
     CREATE EXTENSION IF NOT EXISTS pgcrypto;
 `;
 export { pgcryptoExtension };
+
+
+//These queries below are useless unless database password authentication works
+
+
+const createAuthFunction = `
+    CREATE OR REPLACE FUNCTION action_authenticate(usr text, pwd text)
+    RETURNS boolean
+    AS
+    $$
+    BEGIN
+        RETURN EXISTS(
+            SELECT name FROM accounts WHERE username=usr AND password = crypt(pwd, password));
+    END;
+    $$
+    LANGUAGE plpgsql VOLATILE;
+`;
+export { createAuthFunction };
+
+
+const createActionTable = `
+    CREATE TABLE action(
+        id_ TEXT NOT NULL PRIMARY KEY
+    );
+`;
+export { createActionTable };
+
+
+const createActionJournalTable = `
+    CREATE TABLE action_journal(
+        id_ bigserial primary key,
+        ts_ timestamp not null default now(),
+        user_id_ text not null,
+        action_id_ text not null references action(id_),
+        request_ jsonb not null,
+        response_ jsonb
+    );
+`;
+export { createActionJournalTable };
+
+
+const createActionDispatcher = `
+    CREATE OR REPLACE FUNCTION action_dispatcher_trigger() 
+    RETURNS trigger AS $BODY$
+    DECLARE
+        response jsonb;
+    BEGIN
+        EXECUTE 'SELECT action_' || NEW.action_id_ || '($1, $2, $3)' 
+            INTO response
+            USING NEW.id_, NEW.user_id_, NEW.request_;
+        NEW.response_ = response;
+        RETURN NEW;
+    END;
+    $BODY$
+    LANGUAGE plpgsql VOLATILE;
+`;
+export { createActionDispatcher };
+
+
+const createDispatcher = `
+    CREATE TRIGGER dispatcher
+    BEFORE INSERT
+    ON action_journal
+    FOR EACH ROW
+    EXECUTE PROCEDURE action_dispatcher_trigger();
+`;
+export { createDispatcher };
--- src/database/setupDB.js
+++ src/database/setupDB.js
@@ -10,8 +10,9 @@ const read = readline.createInterface({
     output: process.stdout
 });
 
-import { accountTableStatusQuery, createAccountTableQuery } from './queries.js';
+import { accountTableStatusQuery, createAccountTableQuery, createAuthFunction } from './queries.js';
 import { addAccountQuery, deleteAccountsTable, pgcryptoExtension } from './queries.js';
+import { createActionTable, createActionDispatcher, createDispatcher, createActionJournalTable } from './queries.js';
 
 
 const deleteDBStr = 'The table `accounts` already exists, meaning this database is already setup.';
@@ -72,6 +73,132 @@ function clearDatabase() {
 }
 
 
+/* Actions begin */
+function createAuthenticationFunction() {
+    client.query(createAuthFunction, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+}
+
+
+function dropAuth() {
+    client.query("DROP FUNCTION action_authenticate;", (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Auth dropped");
+    });
+}
+
+
+function dropActionTable() {
+    client.query("DROP TABLE action;", (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Actions table dropped");
+    });
+}
+
+
+function createActionsTable() {
+    client.query(createActionTable, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Actions table created");
+    });
+}
+
+
+function createDispatcherTrigger() {
+    client.query(createDispatcher, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Dispatch trigger created");
+    });
+}
+
+
+function createDispatcherFunction() {
+    client.query(createActionDispatcher, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Dispatch function created");
+    });
+}
+
+
+function makeActionsPublic() {
+    client.query("INSERT INTO action VALUES ('authentication');", (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Actions public now");
+    });
+}
+
+
+function dropTriggerDispatcher() {
+    client.query("DROP TRIGGER dispatcher ON action_journal;", (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Dispatcher trigger dropped");
+    });
+}
+
+
+function dropActionJournal() {
+    client.query("DROP TABLE action_journal;", (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("action_journal dropped");
+    });
+}
+
+
+function createActionJournal() {
+    client.query(createActionJournalTable, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("action_journal created");
+    });
+}
+
+
+async function actions() {
+    await dropAuth();
+    await dropTriggerDispatcher();
+    await dropActionJournal();
+    await dropActionTable();
+
+    await createActionsTable();
+    await createActionJournal();
+    await createDispatcherFunction();
+    await createDispatcherTrigger();
+    await createAuthenticationFunction();
+    await makeActionsPublic();
+}
+/* Actions end */
+
+
 function recreateAccountTable() {
     read.question(deleteDBStr + '\n' + warning, response => {
         if (response === "drop-database") {
@@ -97,6 +224,7 @@ function testAccountTableExists() {
 
 
 async function callQueries() {
+    //await actions();   //This function creates public functions on a table called actions for more security - needs more work
     await createPgcryptoExtension();
     await testAccountTableExists();
 }
